aad709eb752a809ace9c1fe231d10f2eeae64040
johngu_mac <johngumaster@outlook.com>
2021-01-19 10:18:58 +0800

    update core and add tests

---
+++ test/test_widget/root_inherited_widget.cpp
@@ -0,0 +1,48 @@
+#include "async_runtime.h"
+
+struct MyWidget : StatefulWidget
+{
+    Object::Ref<State> createState() override;
+};
+
+struct _MyWidgetState : State<MyWidget>
+{
+    using super = ::State<MyWidget>;
+    Object::Ref<Timer> _timer;
+
+    void _requestExit()
+    {
+        if (this->getMounted())
+        {
+            Logger::of(this->getContext())->writeLine("MyWidget request to exit");
+            RootInheritedWidget::of(this->getContext())->requestExit();
+        }
+    }
+
+    void initState() override
+    {
+        super::initState();
+        auto self = Object::self(this);
+        _timer = Timer::delay(
+            this, [self] { self->_requestExit(); }, Duration(2000));
+    }
+
+    void dispose() override
+    {
+        _timer->dispose();
+        super::dispose();
+    }
+
+    Object::Ref<Widget> build(Object::Ref<BuildContext>) override
+    {
+        return LeafWidget::factory();
+    }
+};
+
+Object::Ref<StatefulWidget::State> MyWidget::createState() { return Object::create<_MyWidgetState>(); }
+
+int main()
+{
+    runApp(Object::create<MyWidget>());
+    return EXIT_SUCCESS;
+}
---
+++ test/test_widget/value_listenable_builder.cpp
@@ -0,0 +1,52 @@
+#include "async_runtime.h"
+
+struct MyWidget : StatefulWidget
+{
+    Object::Ref<State> createState() override;
+};
+
+struct _MyWidgetState : State<MyWidget>
+{
+    using super = ::State<MyWidget>;
+    Object::Ref<Timer> _timer;
+    Object::Ref<ValueNotifier<bool>> _notifier;
+
+    void initState() override
+    {
+        super::initState();
+        auto self = Object::self(this);
+        _notifier = Object::create<ValueNotifier<bool>>(false);
+        _timer = Timer::periodic(
+            this,
+            [self] {
+                self->_notifier->setValue(!self->_notifier->getValue());
+                if (self->_notifier->getValue() == false)
+                    RootInheritedWidget::of(self->getContext())->requestExit();
+            },
+            Duration(2000));
+    }
+
+    void dispose() override
+    {
+        _timer->dispose();
+        super::dispose();
+    }
+
+    Object::Ref<Widget> build(Object::Ref<BuildContext>) override
+    {
+        return Object::create<ValueListenableBuilder<bool>>(
+            [](Object::Ref<BuildContext> context, bool value, Object::Ref<Widget> child) {
+                debug_print("current value: " << value);
+                return LeafWidget::factory();
+            },
+            _notifier);
+    }
+};
+
+Object::Ref<StatefulWidget::State> MyWidget::createState() { return Object::create<_MyWidgetState>(); }
+
+int main()
+{
+    runApp(Object::create<MyWidget>());
+    return EXIT_SUCCESS;
+}
