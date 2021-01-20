89d75856ed1172434eb00b784d8a604fd3639352
johngu_mac <johngumaster@outlook.com>
2021-01-20 15:35:33 +0800

    update core and test case

--- test/main_activity.h
+++ test/main_activity.h
@@ -8,12 +8,11 @@ class MainActivity : public StatelessWidget
 {
     Object::Ref<Widget> build(Object::Ref<BuildContext> context) override
     {
-        // Don't declare as static when access the context!
-        Object::List<Object::Ref<Widget>> children = {
-            Object::create<PeriodicOutput>(LeafWidget::factory(), Logger::of(context)), // PeriodicOutput
-            Object::create<NotificationTest>(),                                         // NotificationTest
-            Object::create<RebuildTest>(),                                              // RebuildTest
-        };
-        return Object::create<LoggerBlocker>(Object::create<MultiChildWidget>(children));
+        return Object::create<LoggerBlocker>(
+            Object::create<MultiChildWidget>()->setChildren({
+                Object::create<PeriodicOutput>(LeafWidget::factory(), Logger::of(context)), // PeriodicOutput
+                Object::create<NotificationTest>(),                                         // NotificationTest
+                Object::create<RebuildTest>(),                                              // RebuildTest
+            }));
     }
 };
--- test/test_widget/my_widget.cpp
+++ test/test_widget/my_widget.cpp
@@ -4,12 +4,13 @@ int main()
 {
     Object::Ref<RootElement> root = Object::create<RootElement>(Object::create<MainActivity>());
     root->attach();
+
     std::this_thread::sleep_for(std::chrono::seconds(2));
     root->onCommand("ls");
 
     std::this_thread::sleep_for(std::chrono::seconds(2));
     root->onCommand("reassembly");
-    
+
     std::this_thread::sleep_for(std::chrono::seconds(2));
     root->detach();
     return EXIT_SUCCESS;
