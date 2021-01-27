a3f109af605dcf32da8fc66d94b50e262b5c080b
johngu_mac <johngumaster@outlook.com>
2021-01-27 09:51:38 +0800

    update File test case

--- test/test_fundamental/file.cpp
+++ test/test_fundamental/file.cpp
@@ -18,16 +18,26 @@ struct _MyWidgetState : State<MyWidget>
         auto self = Object::cast<>(this);
         _file = File::fromPath(this, "app.log");
         _timer = Timer::delay(
-            this, [self] {
-                self->_file->overwrite("This is app log. \n")
+            this,
+            [self] {
+                self->_file->exists()
+                    ->than<void>([self](const bool &exists) { debug_print("File exists: " << exists); })
                     ->than<void>([self] {
-                        self->_file->readWordAsStream()
-                            ->listen([self](String word) { debug_print(word); })
-                            ->onClose([self] {
-                                self->_file->read()
-                                    ->than<void>([self](const String &value) {
-                                        debug_print(value);
-                                        RootInheritedWidget::of(self->context)->requestExit();
+                        self->_file->overwrite("This is app log. \n")
+                            ->than<void>([self] {
+                                self->_file->readWordAsStream()
+                                    ->listen([self](String word) { debug_print(word); })
+                                    ->onClose([self] {
+                                        self->_file->read()
+                                            ->than<void>([self](const String &value) {
+                                                debug_print(value);
+                                                debug_print("To remove file: " << self->_file->path);
+                                                self->_file->remove()->than<void>(
+                                                    [self](const int &code) {
+                                                        debug_print("Remove code: " << code);
+                                                        RootInheritedWidget::of(self->context)->requestExit();
+                                                    });
+                                            });
                                     });
                             });
                     });
