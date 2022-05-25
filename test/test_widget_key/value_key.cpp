617b524e1ba90e68edc354c717f19f31344c6faf
JohnGu9 <31389215+JohnGu9@users.noreply.github.com>
2022-05-25 11:43:41 +0800

    add ValueKey test case

---
+++ test/test_widget_key/value_key.cpp
@@ -0,0 +1,52 @@
+#include "value_key.h"
+
+class MyWidget : public StatefulWidget
+{
+    ref<State<>> createState() override;
+};
+
+class _MyWidgetState : public State<MyWidget>
+{
+    using super = State<MyWidget>;
+    ref<String> text = "text";
+
+    void initState() override
+    {
+        super::initState();
+        Future<int>::delay(1000, 0)
+            ->then([this] {       //
+                setState([this] { //
+                    std::cout << "not change ValueKey's value" << std::endl;
+                    text = "text"; // not change value, just only update widget without rebuild the subtree
+                });
+            })
+            ->then<int>([]
+                        { return Future<int>::delay(1000, 0); })
+            ->then([this] {       //
+                setState([this] { //
+                    std::cout << "change ValueKey's value" << std::endl;
+                    text = "new text"; // change the ValueKey's value to force system rebuild the subtree and mount a new _ChildState
+                });
+            })
+            ->then<int>([]
+                        { return Future<int>::delay(1000, 0); })
+            ->then([this]
+                   { RootWidget::of(context)->exit(); });
+    }
+
+    ref<Widget> build(ref<BuildContext>) override
+    {
+        return Object::create<Child>(Object::create<ValueKey<ref<String>>>(text));
+    }
+};
+
+inline ref<State<>> MyWidget::createState()
+{
+    return Object::create<_MyWidgetState>();
+}
+
+int main()
+{
+    runApp(Object::create<MyWidget>());
+    return 0;
+}
