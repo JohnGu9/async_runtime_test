c8d510da5e7bede8d1806d376627d48adae187e8
JohnGu9 <31389215+JohnGu9@users.noreply.github.com>
2022-06-17 15:59:52 +0800

    add MapIterable test case

---
+++ test/test_object/map_iterable.cpp
@@ -0,0 +1,26 @@
+#include "async_runtime/object/object.h"
+#include <algorithm>
+#include <assert.h>
+
+class A : public Object
+{
+public:
+    const int value;
+    A(int value) : value(value) {}
+};
+
+int main()
+{
+    ref<List<ref<A>>> list = {
+        Object::create<A>(1),
+        Object::create<A>(2),
+        Object::create<A>(3),
+    };
+    auto mapped = MapIterable<ref<A>, int>::from(list, [](const ref<A> &origin)
+                                                 { return origin->value; });
+    auto expect = {1, 2, 3};
+    assert(std::equal(mapped->begin(), mapped->end(), expect.begin()));
+    std::cout << mapped << std::endl;
+
+    return EXIT_SUCCESS;
+}
