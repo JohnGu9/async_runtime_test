5092f82dcfabbf3c5351a3b1486a2e92734964c1
JohnGu9 <31389215+JohnGu9@users.noreply.github.com>
2022-06-23 16:49:29 +0800

    update map test case

--- test/test_object/map.cpp
+++ test/test_object/map.cpp
@@ -4,10 +4,14 @@
 
 static void testBasic();
 static void testMerge();
+static void testLateInit();
+static void testMixKey();
 int main()
 {
     testBasic();
     testMerge();
+    testLateInit();
+    testMixKey();
 }
 
 static void testBasic()
@@ -67,7 +71,40 @@ static void testMerge()
     auto merge0 = Map<int, int>::merge({map0, map1});
     assert(merge0->size() == 5);
     for (const auto &key : {1, 2, 3, 4, 5})
+    {
         assert(merge0->containsKey(key));
+        assert(merge0[key] == key);
+    }
     assert(!merge0->containsKey(6));
     std::cout << merge0 << std::endl;
+}
+
+static void testLateInit()
+{
+    lateref<Map<int, ref<String>>> map;
+    map = Map<int, ref<String>>::create();
+    map = {{1, "1"}, {2, "2"}};
+    assert(map[1] == "1");
+    assert(map->size() == 2);
+    std::cout << map << std::endl;
+}
+
+class A : public Object
+{
+};
+static void testMixKey()
+{
+    ref<Map<ref<Object>, int>> map = Map<ref<Object>, int>::create();
+    map[ref<String>("1")] = 1;
+    map[Object::create<A>()] = 123;
+    map[ref<String>("2")] = 2;
+    assert(map->size() == 3);
+
+    std::cout << map << std::endl;
+
+    map[ref<String>("2")] = 4;
+    map[Object::create<A>()] = 321;
+    assert(map->size() == 4);
+
+    std::cout << map << std::endl;
 }
