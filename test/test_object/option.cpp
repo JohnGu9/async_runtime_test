71f60eefbcea5189396b3581dbbdf5e8ad331aab
JohnGu9 <31389215+JohnGu9@users.noreply.github.com>
2022-06-30 09:13:58 +0800

    add option test case

---
+++ test/test_object/option.cpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "async_runtime/object/object.h"
+#include <assert.h>
+
+class A : public Object
+{
+};
+
+int main()
+{
+    ref<A> r = Object::create<A>();
+
+    option<A> f = r;
+    option<A> e = nullptr;
+    assert(!Object::isNull<>(f));
+    assert(Object::isNull<>(e));
+
+    e = f;
+    f = nullptr;
+    assert(Object::isNull<>(f));
+    assert(!Object::isNull<>(e));
+
+    std::cout << f << std::endl
+              << e << std::endl
+              << f.ifNotNullElse([&]
+                                 { return r; })
+              << std::endl;
+}
