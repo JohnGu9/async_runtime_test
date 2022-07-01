0d70217f1c246bcb540311f641a34e4f8708b545
JohnGu9 <31389215+JohnGu9@users.noreply.github.com>
2022-07-01 13:52:36 +0800

    add more test on Number

--- test/test_utilities/number.cpp
+++ test/test_utilities/number.cpp
@@ -1,5 +1,7 @@
 #include "async_runtime/utilities/number.h"
 
+static void integer_test();
+static void float_test();
 int main()
 {
     ref<Number> i = 10;
@@ -15,4 +17,26 @@ int main()
         {key, value},
     };
     std::cout << map << std::endl;
+
+    integer_test();
+    float_test();
+}
+
+static void integer_test()
+{
+    ref<Number> s = (short)10;
+    ref<Number> i = 10;
+    ref<Number> l = 10l;
+    ref<Number> ll = 10ll;
+
+    assert(i == s);
+    assert(i == l);
+    assert(i == ll);
+}
+
+static void float_test()
+{
+    ref<Number> f = 10.0f;
+    ref<Number> d = 10.0;
+    assert(f == d);
 }
