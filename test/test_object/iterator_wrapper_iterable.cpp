8965c5a52ba358b6ca84a54a3d771ac3827b967a
JohnGu9 <31389215+JohnGu9@users.noreply.github.com>
2022-08-04 16:20:57 +0800

    update IteratorWrapperIterable test case

--- test/test_object/iterator_wrapper_iterable.cpp
+++ test/test_object/iterator_wrapper_iterable.cpp
@@ -2,16 +2,24 @@
 #include <algorithm>
 #include <array>
 #include <assert.h>
+#include <list>
 
 int main()
 {
     std::array<int, 5> array = {1, 2, 3, 4, 5};
+    std::list<int> link = {1, 2, 3, 4, 5};
     auto list = {1, 2, 3, 4, 5};
-    auto wrapper0 = IteratorWrapperIterable<int, std::array<int, 5>::iterator>::from(array.begin(), array.end());
-    auto wrapper1 = IteratorWrapperIterable<int, std::initializer_list<int>::iterator>::from(list.begin(), list.end());
+    auto wrapper0 = IteratorWrapperIterable<>::from(array.begin(), array.end());
+    auto wrapper1 = IteratorWrapperIterable<>::from(array);
+    auto wrapper2 = IteratorWrapperIterable<>::from(link);
+    auto wrapper3 = IteratorWrapperIterable<>::from(static_cast<const std::array<int, 5> &>(array));
+    auto wrapper4 = IteratorWrapperIterable<>::from(list);
     assert(std::equal(wrapper0->begin(), wrapper0->end(), wrapper1->begin()));
     std::cout << wrapper0 << std::endl
-              << wrapper1 << std::endl;
+              << wrapper1 << std::endl
+              << wrapper2 << std::endl
+              << wrapper3 << std::endl
+              << wrapper4 << std::endl;
 
     return EXIT_SUCCESS;
 }
